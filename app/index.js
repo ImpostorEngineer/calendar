b6ded136a1727276dcb36bd01c9881e58ea10cbb
ImpostorEngineer <posta.gereksiz@gmail.com>
2022-12-14 11:38:35 -0500

    fix vercel #34551324

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const api = require('./app/app');
+const api = require('./app');
 const app = express();
 
 app.use(express.static('public'));
 app.use('/api', api);
 app.use(express.json());
 
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
